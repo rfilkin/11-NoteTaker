18c1dc986d6b21ce2bc0a4b56416aaf53be5bb90
Robert Filkin <rfilkin17@gmail.com>
2020-03-20 15:20:32 -0700

    heroku accomodations

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var fs = require("fs");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT||3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -48,7 +48,7 @@ app.get("/", function(req, res) {
 //note: this * one MUST be the LAST of the GET routes listed here, or else the program will shortcut to it.
 //the * makes this a catch-all expression
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 //POST api notes
